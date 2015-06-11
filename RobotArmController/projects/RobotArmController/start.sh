e5de0dbc6b1eba38d7a6f0b43705529998e4df8c
Nobuhiko Miyamoto <TyouKyozyakuTaisitu@yahoo.co.jp>
2015-06-11 20:07:53 +0900

    2007

--- RobotArmController/projects/RobotArmController/start.sh
+++ RobotArmController/projects/RobotArmController/start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd `dirname $0`
+python startNamingService.py
 cd ../../workspace
 rtcd -f ../projects/RobotArmController/Cpp/rtc.conf&
 python ../Manager/Python/rtcd.py -f ../projects/RobotArmController/Python/rtc.conf&
